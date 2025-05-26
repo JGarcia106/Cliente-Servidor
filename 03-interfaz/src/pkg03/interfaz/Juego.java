e720419fd36ff16dbd51c952fbc41089b5846816
JGarcia106 <joelitogarcia696@gmail.com>
2025-05-26 10:37:07 -0600

    Ejemplo interfaz

---
+++ 03-interfaz/src/pkg03/interfaz/Juego.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package pkg03.interfaz;
+
+/**
+ *
+ * @author Joel
+ */
+public interface Juego {
+    
+    public abstract void iniciar();
+    
+    public abstract void jugar();
+    
+    public abstract void finalizar();
+}
